a0633e601090d87e80bb78ad8b59b51903a463da
jd <jashansingh29@gmail.com>
2025-03-15 14:03:52 -0400

    k

--- backend/Processing/ProcessVideoAux.py
+++ backend/Processing/ProcessVideoAux.py
@@ -49,11 +49,11 @@ def process_video(file_path: str):
 
 if __name__ == "__main__":
     VIDEO_PATH = (
-        "/Users/jashan/projects/LaurierAnalitics2025/tests/testImages/Test1.mov"
+        "/Users/jashan/projects/LaurierAnalitics2025/tests/testImages/Test4.mov"
     )
-    cords = fetch_score_coords(VIDEO_PATH)
-    print(cords)
-    print("Stage 2")
-    results = analyze_segment(VIDEO_PATH, cords, 1, "this")
-    print(results)
-    # cords = process_video(VIDEO_PATH)
+    # cords = fetch_score_coords(VIDEO_PATH)
+    # print(cords)
+    # print("Stage 2")
+    # results = analyze_segment(VIDEO_PATH, cords, 1, "this")
+    # print(results)
+    cords = process_video(VIDEO_PATH)
