c2dbbcd1d20fc8da8eeab974ddb6e5a2fa0a652c
jd <jashansingh29@gmail.com>
2025-03-14 22:05:19 -0400

    detectionStageOneCompleted

--- tests/ImageProcessingFunctions.py
+++ tests/ImageProcessingFunctions.py
@@ -1,25 +1,36 @@
 """-------------------------------------------------------
-Get Score Card: Get Coordinates of Score Card
+CLIP GENIUS: Image Processing Functions
 -------------------------------------------------------
 Author:  JD
 ID:      91786
-Uses:    numpy,openCV
+Uses:    numpy,openCV,pytesseract
 Version:  1.0.8
 __updated__ = Fri Mar 14 2025
 -------------------------------------------------------
 """
 
-import cv2 as cv
+# IMPORTS
+from pytesseract import Output
+import pytesseract
 import numpy as np
+import cv2 as cv
 
-# Constants
-THRESHOLD1 = 50
+# CONSTANTS
+THRESHOLD1 = 50  # for canny edge dector
 THRESHOLD2 = 200
-LINE_THRESHOLD = 100
+
+LINE_THRESHOLD = 100  # For Hough Line Transform
 MIN_LINE_LENGTH = 150
 MAX_LINE_GAP = 10
 HORIZONTAL_ANGLE_THRESHOLD = 10
-SCOREBOARD_Y_RANGE = (0.75, 0.90)
+
+SCOREBOARD_Y_RANGE = (0.75, 0.90)  # Expected Area
+
+CONFIG = r"--psm 11 --oem 3"  # for OCR
+CONFIDENCE_THRESHOLD = 40
+
+
+COLOR = (0, 255, 0)
 
 
 def get_scoreboard_coordinates(image: np.ndarray) -> tuple[int, int, int, int]:
@@ -75,3 +86,82 @@ def get_scoreboard_coordinates(image: np.ndarray) -> tuple[int, int, int, int]:
 
 def extract_scoreboard(image: np.ndarray, x1: int, y1: int, x2: int, y2: int):
     return image[y1:y2, x1:x2]
+
+
+def add_timestamp_to_frame(frame, timestamp):
+    timestamp_text = f"Time: {timestamp:.2f}s"
+    cv.putText(
+        frame,
+        timestamp_text,
+        (10, 30),
+        cv.FONT_HERSHEY_SIMPLEX,
+        1,
+        COLOR,
+        2,
+        cv.LINE_AA,
+    )
+
+
+def is_overlap(rect1, rect2):
+    x1, y1, w1, h1 = rect1
+    x2, y2, w2, h2 = rect2
+
+    r1 = x1 + w1
+    b1 = y1 + h1
+    r2 = x2 + w2
+    b2 = y2 + h2
+
+    if (r1 <= x2) or (r2 <= x1) or (b1 <= y2) or (b2 <= y1):
+        return False
+    else:
+        return True
+
+
+def plotscores_on_images(image, scores):
+    for s in scores:
+        image = cv.rectangle(
+            image, (s[0], s[1]), (s[0] + s[2], s[1] + s[3]), COLOR, thickness=2
+        )
+    return image
+
+
+def convert_to_abs_coordinates(x1, y1, scores):
+    if not len(scores) == 2:
+        return False
+    absolute_coordinates = []
+    if not is_overlap(scores[0], scores[1]):
+        for s in scores:
+            absolute_coordinates.append((s[0] + x1, s[1] + y1, s[2], s[3]))
+        return absolute_coordinates
+    return None
+
+
+def find_scores(image: np.ndarray, confidence_threshold: int = CONFIDENCE_THRESHOLD):
+    annotated_image = image.copy()
+
+    # Perform OCR
+    data = pytesseract.image_to_data(
+        annotated_image, config=CONFIG, output_type=Output.DICT
+    )
+    cords = []
+    for i in range(len(data["text"])):
+        text, conf = data["text"][i], float(data["conf"][i])
+        if text.strip() == "0" and conf > confidence_threshold:
+            cords.append(
+                (
+                    data["left"][i],
+                    data["top"][i],
+                    data["width"][i],
+                    data["height"][i],
+                )
+            )
+
+    return cords
+
+
+def extract_scores_location(frame):
+    x1, y1, x2, y2 = get_scoreboard_coordinates(frame)
+    extracted_image = extract_scoreboard(frame, x1, y1, x2, y2)
+    score_cords = find_scores(extracted_image)
+    abs_cords = convert_to_abs_coordinates(x1, y1, score_cords)
+    return abs_cords
--- tests/TestVideo.py
+++ tests/TestVideo.py
@@ -9,48 +9,11 @@ __updated__ = Fri Mar 14 2025
 -------------------------------------------------------
 """
 
-import cv2 as cv
-import numpy as np
 from ImageProcessingFunctions import *
-from test_service2 import *
+
 
 # Constants
 FRAME_SIZE = (1000, 1000)
-COLOR = (0, 255, 0)
-
-
-def is_overlap(rect1, rect2):
-    x1, y1, w1, h1 = rect1
-    x2, y2, w2, h2 = rect2
-
-    r1 = x1 + w1
-    b1 = y1 + h1
-    r2 = x2 + w2
-    b2 = y2 + h2
-
-    if (r1 <= x2) or (r2 <= x1) or (b1 <= y2) or (b2 <= y1):
-        return False
-    else:
-        return True
-
-
-def plotscores_on_images(image, scores):
-    for s in scores:
-        image = cv.rectangle(
-            image, (s[0], s[1]), (s[0] + s[2], s[1] + s[3]), COLOR, thickness=2
-        )
-    return image
-
-
-def convert_to_abs_coordinates(x1, y1, scores):
-    if not len(scores) == 2:
-        return False
-    absolute_coordinates = []
-    if not is_overlap(scores[0], scores[1]):
-        for s in scores:
-            absolute_coordinates.append((s[0] + x1, s[1] + y1, s[2], s[3]))
-        return absolute_coordinates
-    return None
 
 
 def process_video(file_path: str):
@@ -61,36 +24,20 @@ def process_video(file_path: str):
         print("Error: Could not open video file.")
         return
 
+    abs_cords = None
     while video.isOpened():
-
         ret, frame = video.read()
         if not ret:
             break
-
         frame = cv.resize(frame, FRAME_SIZE)
-        x1, y1, x2, y2 = get_scoreboard_coordinates(frame)
-        # cv.rectangle(frame, (x1, y1), (x2, y2), COLOR, 2)
 
-        extracted_image = extract_scoreboard(frame, x1, y1, x2, y2)
-        score_cords = find_scores(extracted_image)
-        abs_cords = convert_to_abs_coordinates(x1, y1, score_cords)
-        if abs_cords:
+        if not abs_cords:
+            abs_cords = extract_scores_location(frame)
+        else:
             frame = plotscores_on_images(frame, abs_cords)
 
-        # TIME STAMP
         timestamp = video.get(cv.CAP_PROP_POS_MSEC) / 1000
-        timestamp_text = f"Time: {timestamp:.2f}s"
-        cv.putText(
-            frame,
-            timestamp_text,
-            (10, 30),
-            cv.FONT_HERSHEY_SIMPLEX,
-            1,
-            (0, 255, 0),
-            2,
-            cv.LINE_AA,
-        )
-
+        add_timestamp_to_frame(frame, timestamp)
         cv.imshow("Scoreboard Detection", frame)
 
         if cv.waitKey(1) & 0xFF == ord("q"):
