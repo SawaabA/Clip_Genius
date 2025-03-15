00f2824716df575a4c3bd22dcb71f4e1a8d3d15d
jd <jashansingh29@gmail.com>
2025-03-15 00:34:37 -0400

    scoreExtraction

--- backend/Processing/ProcessVideoAux.py
+++ backend/Processing/ProcessVideoAux.py
@@ -44,10 +44,55 @@ def process_video(file_path: str):
     cv.destroyAllWindows()
 
 
+def get_score_value(frame: np.ndarray, coords: list[tuple[int, int, int, int]]) -> str:
+    x1, y1, w1, h1 = coords[0]
+    x2, y2, w2, h2 = coords[1]
+    score_region = frame[y1 - 5 : y2 + h2 + 5, x1 - 5 : x2 + w2 + 5]
+
+    gray = cv.cvtColor(score_region, cv.COLOR_BGR2GRAY)
+
+    gray = cv.resize(gray, None, fx=2, fy=2, interpolation=cv.INTER_LINEAR)
+
+    gray = cv.GaussianBlur(gray, (5, 5), 0)
+    _, binary = cv.threshold(gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+
+    custom_config = r"--oem 3 --psm 6"
+    extracted_text = pytesseract.image_to_string(binary, config=custom_config).strip()
+
+    print(extracted_text)
+    return binary
+
+
+def analyze_segment(file_path, score_coords, segment_number, masterfile):
+    video = cv.VideoCapture(file_path)
+    if not video.isOpened():
+        print("Error: Could not open video file.")
+        return
+
+    curr_value = 0
+    while video.isOpened():
+        ret, frame = video.read()
+        if not ret:
+            break
+        frame = cv.resize(frame, FRAME_SIZE)
+        frame = get_score_value(frame, score_coords)
+        cv.imshow("Scoreboard Detection", frame)
+
+        if cv.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    video.release()
+    cv.destroyAllWindows()
+    return None
+
+
 if __name__ == "__main__":
     VIDEO_PATH = (
         "/Users/jashan/projects/LaurierAnalitics2025/tests/testImages/Test1.mov"
     )
     cords = fetch_score_coords(VIDEO_PATH)
+    print(cords)
+    print("Stage 2")
+    analyze_segment(VIDEO_PATH, cords, 1, "this")
     # cords = process_video(VIDEO_PATH)
     print(cords)
--- tests/test_service2.py
+++ tests/test_service2.py
@@ -1,1 +1,24 @@
-# Constants
+def get_score_value(frame, cords):
+    x1, y1, width1, height1 = cords[0]
+    cropped_image_1 = frame[y1 : y1 + height1, x1 : x1 + width1]
+
+    x2, y2, width2, height2 = cords[1]
+    cropped_image_2 = frame[y2 : y2 + height2, x2 : x2 + width2]
+
+    height = min(cropped_image_1.shape[0], cropped_image_2.shape[0])
+    widht = min(cropped_image_1.shape[1], cropped_image_2.shape[1])
+
+    cropped_image_1 = cv.resize(cropped_image_1, (height, widht))
+    cropped_image_2 = cv.resize(cropped_image_2, (height, widht))
+
+    combined = np.hstack((cropped_image_1, cropped_image_2))
+
+    gray = cv.cvtColor(combined, cv.COLOR_BGR2GRAY)
+    _, binary = cv.threshold(gray, 128, 255, cv.THRESH_BINARY_INV)
+    binary = cv.resize(binary, None, fx=2, fy=2, interpolation=cv.INTER_LINEAR)
+
+    custom_config = r"--oem 3 --psm 10"
+    char = pytesseract.image_to_string(binary, config=custom_config)
+    char = char.strip()
+    print(char)
+    return combined
